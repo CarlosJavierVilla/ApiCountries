192e0cccd638f54bdb3c27bfc7e6283756eb267b
javier <villarestrepocarlos@gmail.com>
2023-03-06 19:23:11 +0100

    Initial commit

---
+++ src/test/java/com/API/Countries/CountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.Countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
